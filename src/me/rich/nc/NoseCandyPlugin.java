98375ea9c192933ba6c22b23c0c73b62c9dbae9f
CandyCaine <candycainenet@gmail.com>
2012-08-25 17:14:51 +0100

    Registered command executors.  Repeating sync tasks now start

--- src/me/rich/nc/NoseCandyPlugin.java
+++ src/me/rich/nc/NoseCandyPlugin.java
@@ -1,12 +1,19 @@
 package me.rich.nc;
 
+import me.rich.nc.executor.AddictionExecutor;
+import me.rich.nc.executor.NoseCandyExecutor;
+import me.rich.nc.executor.SpawnDrugExecutor;
 import me.rich.nc.listener.NoseCandyPlayerListener;
 import me.rich.nc.persistence.Configuration;
 import me.rich.nc.persistence.Database;
+import me.rich.nc.util.runnable.CocaineAddictionRunnable;
+import me.rich.nc.util.runnable.HIVRunnable;
+import me.rich.nc.util.runnable.HepatitisRunnable;
 
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitScheduler;
 import org.getspout.spoutapi.SpoutManager;
 import org.getspout.spoutapi.player.FileManager;
 
@@ -32,20 +39,32 @@ public class NoseCandyPlugin extends JavaPlugin {
 	public void onEnable() {
 		NoseCandyPlugin.instance = this;
 		this.database = new Database(this, "NoseCandy");
-		
-		database.createNewPlayerRecord("test");
-
 		this.config = new Configuration(this);
 		this.addToCache();
+		this.registerCommands();
 		this.customItems = new CustomItems(this);
 		Bukkit.getServer().getPluginManager().registerEvents(new NoseCandyPlayerListener(this), this);
+		this.startTasks();
 	}
 
 	private void addToCache() {
 		FileManager fm = SpoutManager.getFileManager();
 		fm.addToPreLoginCache(this, "https://dl.dropbox.com/u/86467984/NoseCandy/Image/bud.png");
 	}
+	
+	private void registerCommands() {
+		this.getCommand(NoseCandyExecutor.name).setExecutor(new NoseCandyExecutor(this));
+		this.getCommand(AddictionExecutor.name).setExecutor(new AddictionExecutor(this));
+		this.getCommand(SpawnDrugExecutor.name).setExecutor(new SpawnDrugExecutor(this));
+	}
 
+	private void startTasks() {
+		BukkitScheduler scheduler = Bukkit.getScheduler();
+		scheduler.scheduleSyncRepeatingTask(this, new HepatitisRunnable(this), 6000L, 6000L);
+		scheduler.scheduleSyncRepeatingTask(this, new HIVRunnable(this), 12000L, 6000L);
+		scheduler.scheduleSyncRepeatingTask(this, new CocaineAddictionRunnable(this), 12000L, 12000L);
+	}
+	
 	public Database getNCDatabase() {
 		return this.database;
 	}
