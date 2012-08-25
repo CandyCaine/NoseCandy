f7c286203f58fc5d2f530c2ad7b3a21b2b3d9846
CandyCaine <candycainenet@gmail.com>
2012-08-25 17:21:38 +0100

    Updated runnables

--- src/me/rich/nc/util/runnable/CocaineAddictionRunnable.java
+++ src/me/rich/nc/util/runnable/CocaineAddictionRunnable.java
@@ -1,6 +1,11 @@
 package me.rich.nc.util.runnable;
 
 import me.rich.nc.NoseCandyPlugin;
+import me.rich.nc.persistence.Database.Column;
+import me.rich.nc.util.EffectFactory;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public class CocaineAddictionRunnable extends GeneralAddictionRunnable {
 
@@ -10,6 +15,11 @@ public class CocaineAddictionRunnable extends GeneralAddictionRunnable {
 
 	@Override
 	public void run() {
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			if (plugin.getNCDatabase().getAddiction(player.getName(), Column.COCAINE_ADDICTION) > 30) {
+				EffectFactory.getInstance().applyCocaineAddictEffect(player);
+			}
+		}
 	}
 
 }
--- src/me/rich/nc/util/runnable/HIVRunnable.java
+++ src/me/rich/nc/util/runnable/HIVRunnable.java
@@ -4,20 +4,22 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import me.rich.nc.NoseCandyPlugin;
+import me.rich.nc.util.EffectFactory;
 
 public class HIVRunnable implements Runnable {
 
 	private NoseCandyPlugin plugin;
-	
+
 	public HIVRunnable(NoseCandyPlugin plugin) {
 		this.plugin = plugin;
 	}
-	
+
 	@Override
 	public void run() {
 		for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-			if (this.plugin.getNCDatabase().hasHepatitis(player.getName())) {
-				
+			if (this.plugin.getNCDatabase().hasHIV(player.getName())) {
+				EffectFactory.getInstance().applyHivEffect(player);
+				player.sendMessage("You have HIV.");
 			}
 		}
 	}
